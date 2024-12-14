8b733117da21dbe0e8e04ca49b5b6c1252094637
Reza Mansouri <reza.mansouri70@gmail.com>
2024-12-14 23:52:33 +0330

    log 5

--- TelegramBotApp/Program.cs
+++ TelegramBotApp/Program.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ContractResolver = new DefaultContractResolver
+    {
+        NamingStrategy = new CamelCaseNamingStrategy()
+    };
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
